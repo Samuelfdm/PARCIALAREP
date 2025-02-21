5562c9d438132ba430fe44fc97ec93cf6cb2f91a
unknown <samuel.diaz-m@labinfo.is.escuelaing.edu.co>
2025-02-21 12:59:59 -0500

    final

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 ****PARCIAL DE AREP****
+
+abra dos consolas, una para FacadeWeb y otro para reflective
+
+Ejecute con java -cp "target/classes" edu.eci.arep.FacadeWeb  
+y la otra con java -cp "target/classes" edu.eci.arep.ReflectiveChatGPT
+
+clone y pasese a la carpeta \arep
+
+
+Intente utiliza Gson para las respuestas json pero java no las encuentra porque me falta clonar las dependencias en el pom
+si agrego esas lineas, funcionan.
--- src/main/java/edu/eci/arep/ReflectiveChatGPT.java
+++ src/main/java/edu/eci/arep/ReflectiveChatGPT.java
@@ -50,21 +50,21 @@ public class ReflectiveChatGPT {
             URI requestURI = new URI(requestStringURI);
             String query = requestURI.getQuery(); //SOLO NECESITO LA QUERY DE LA FUNCION PA SABER Q ES
             //Utilizo JsonObject para facilidad de las respuestas
-            JsonObject response;
+            String response;
             if (query != null && query.startsWith("comando=")) {
                 String comando = query.split("=")[1]; //AQUI PARTIMOS LA QUERY EN DOS PARA OBTENER [1] SOLO LA FUNCION
                 response = procesarComando(comando);
             } else {
                 JsonObject errorResponse = new JsonObject();
                 errorResponse.addProperty("error", "Errorrrrr ");
-                response = errorResponse;
+                response = String.valueOf(errorResponse);
             }
 
             //respuesta HTTP en formato JSON
             String jsonResponse = "HTTP/1.1 200 OK\r\n" +
                     "Content-Type: application/json\r\n" +
                     "\r\n" +
-                    response.toString();
+                    response;
 
             // Enviamos la respuesta al cliente
             out.println(jsonResponse);
@@ -75,12 +75,11 @@ public class ReflectiveChatGPT {
         serverSocket.close();
     }
 
-    private static JsonObject procesarComando(String comando) {
+    private static String procesarComando(String comando) {
         try {
             String reflectiveMethod = comando.split("\\(")[0]; //OBTENEMOS EL NOMBRE DEL METODO GENERAL A EJECUTAR,EJ: Class, invoke, unaryInvoke, binaryInvoke
             String params = comando.split("\\(")[1].split("\\)")[0];//OBTENEMOS LOS PARAMETROS DENTRO DE LOS PARENTESIS (,,,)
             String[] paramsArray = params.split(",");//SEPARAMOS LOS PARAMETROS Y LOS GUARDAMOS EN UN ARREGLO PARA FACILIDAD
-
             Class<?> clazz = Class.forName(paramsArray[0]); //Obtenemos un objeto de tipo clase que representa la clase a llamar dentro de los parametros generales, EJ: java.lang.Math, java.lang.System
             String methodName = (paramsArray.length > 1) ? paramsArray[1] : "";//Metodo de la clase dentro de los parametros que queremos ejecutar, EJ: max, sum, abs, getenv
 
@@ -89,21 +88,113 @@ public class ReflectiveChatGPT {
                 case "Class":
                     return obtenerTodo(clazz);
                 case "invoke":
+                    //return "invoke";
                     return invocarMetodo(clazz, methodName);
                 case "unaryInvoke":
-                    return invocarMetodoUnario(clazz, methodName, paramsArray[2], paramsArray[3]);
+                    return "invoke";
+                    //return invocarMetodoUnario(clazz, methodName, paramsArray[2], paramsArray[3]);
                 case "binaryInvoke":
-                    return invocarMetodoBinario(clazz, methodName, paramsArray[2], paramsArray[3], paramsArray[4], paramsArray[5]);
+                    return "invoke";
+                    //return invocarMetodoBinario(clazz, methodName, paramsArray[2], paramsArray[3], paramsArray[4], paramsArray[5]);
                 default:
-                    JsonObject errorResponse = new JsonObject();
-                    errorResponse.addProperty("error", "Comando no reconocido");
-                    return errorResponse;
+                    return "invoke";
+                    //JsonObject errorResponse = new JsonObject();
+                    //errorResponse.addProperty("error", "Comando no reconocido");
+                    //return errorResponse;
             }
         } catch (Exception e) {
             JsonObject errorResponse = new JsonObject();
             errorResponse.addProperty("error", "Errorrrrr: " + e.getMessage());
+            return "errrororororor";
+        }
+    }
+    //Class([class name])
+    private static String obtenerTodo(Class<?> clazz) {
+        StringBuffer response = new StringBuffer();
+        response.append(clazz.getName());
+
+        List<Object> methodsList = new ArrayList<>();
+        for (Method method : clazz.getDeclaredMethods()) {
+            methodsList.add(method.getName());
+        }
+        response.append(methodsList.toString());
+
+        List<Object> fieldsList = new ArrayList<>();
+        for (Field field : clazz.getDeclaredFields()) {
+            fieldsList.add(field.getName());
+        }
+        response.append(fieldsList.toString());
+
+        return response.toString();
+    }
+   //invoke([class name],[method name])
+    private static String invocarMetodo(Class<?> clazz, String methodName) {
+        try {
+            Method method = clazz.getDeclaredMethod(methodName);
+            Object result = method.invoke(null);
+            StringBuffer response = new StringBuffer();
+            response.append(result.toString());
+            return response.toString();
+        } catch (Exception e) {
+            JsonObject errorResponse = new JsonObject();
+            errorResponse.addProperty("error", "Error al invocar método " + e.getMessage());
+            return "errorResponse";
+        }
+    }
+
+        //unaryInvoke([class name],[method name],[paramtype],[param value])
+    private static JsonObject invocarMetodoUnario(Class<?> clazz, String methodName, String paramType, String paramValue) {
+        try {
+            Method method = clazz.getDeclaredMethod(methodName, obtenerTipo(paramType));
+            Object param = obtenerValor(paramType, paramValue);
+            Object result = method.invoke(null, param);
+            JsonObject response = new JsonObject();
+            response.addProperty("result", result.toString());
+            return response;
+        } catch (Exception e) {
+            JsonObject errorResponse = new JsonObject();
+            errorResponse.addProperty("error", "Error al invocar método" + e.getMessage());
             return errorResponse;
         }
     }
+
+    /*
+   //binaryInvoke([class name],[method name],[paramtype 1],[param value], [paramtype 2],[param value 2])
+    private static JsonObject invocarMetodoBinario(Class<?> clazz, String methodName, String paramType1, String paramValue1, String paramType2, String paramValue2) {
+        try {
+            Method method = clazz.getDeclaredMethod(methodName, obtenerTipo(paramType1), obtenerTipo(paramType2));
+            Object param1 = obtenerValor(paramType1, paramValue1);
+            Object param2 = obtenerValor(paramType2, paramValue2);
+            Object result = method.invoke(null, param1, param2);
+            JsonObject response = new JsonObject();
+            response.addProperty("result", result.toString());
+            return response;
+        } catch (Exception e) {
+            JsonObject errorResponse = new JsonObject();
+            errorResponse.addProperty("error", "Error al invocar método " + e.getMessage());
+            return errorResponse;
+        }
+    }
+
+     */
+
+    private static Class<?> obtenerTipo(String type) {
+        switch (type) {
+            case "int": return int.class;
+            case "double": return double.class;
+            case "String": return String.class;
+            default: return null;
+        }
+    }
+
+    private static Object obtenerValor(String type, String value) {
+        switch (type) {
+            case "int": return Integer.parseInt(value);
+            case "double": return Double.parseDouble(value);
+            case "String": return value;
+            default: return null;
+        }
+    }
+
 
 }
