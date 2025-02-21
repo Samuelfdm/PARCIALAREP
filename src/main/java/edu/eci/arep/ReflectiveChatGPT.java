f8902302b21259eb18da1c5d5e614dbc077c613d
unknown <samuel.diaz-m@labinfo.is.escuelaing.edu.co>
2025-02-21 12:28:10 -0500

    correccion y uso de jsons

--- src/main/java/edu/eci/arep/ReflectiveChatGPT.java
+++ src/main/java/edu/eci/arep/ReflectiveChatGPT.java
@@ -49,20 +49,22 @@ public class ReflectiveChatGPT {
             String requestStringURI = requestLine.split(" ")[1];
             URI requestURI = new URI(requestStringURI);
             String query = requestURI.getQuery(); //SOLO NECESITO LA QUERY DE LA FUNCION PA SABER Q ES
-
-            String response;
+            //Utilizo JsonObject para facilidad de las respuestas
+            JsonObject response;
             if (query != null && query.startsWith("comando=")) {
                 String comando = query.split("=")[1]; //AQUI PARTIMOS LA QUERY EN DOS PARA OBTENER [1] SOLO LA FUNCION
                 response = procesarComando(comando);
             } else {
-                response = "ERROR EN EL FORMATO DEL COMANDO";
+                JsonObject errorResponse = new JsonObject();
+                errorResponse.addProperty("error", "Errorrrrr ");
+                response = errorResponse;
             }
 
             //respuesta HTTP en formato JSON
             String jsonResponse = "HTTP/1.1 200 OK\r\n" +
                     "Content-Type: application/json\r\n" +
                     "\r\n" +
-                    response;
+                    response.toString();
 
             // Enviamos la respuesta al cliente
             out.println(jsonResponse);
@@ -73,17 +75,34 @@ public class ReflectiveChatGPT {
         serverSocket.close();
     }
 
-    private static String procesarComando(String comando){
-        if(comando.contains("Class")){
-            return "Class";
-        } else if(comando.contains("invoke")){
-            return "invoke";
-        } else if(comando.contains("unaryInvoke")){
-            return "unaryInvoke";
-        } else if(comando.contains("binaryInvoke")){
-            return "binaryInvoke";
-        } else {
-            return "ELSE";
+    private static JsonObject procesarComando(String comando) {
+        try {
+            String reflectiveMethod = comando.split("\\(")[0]; //OBTENEMOS EL NOMBRE DEL METODO GENERAL A EJECUTAR,EJ: Class, invoke, unaryInvoke, binaryInvoke
+            String params = comando.split("\\(")[1].split("\\)")[0];//OBTENEMOS LOS PARAMETROS DENTRO DE LOS PARENTESIS (,,,)
+            String[] paramsArray = params.split(",");//SEPARAMOS LOS PARAMETROS Y LOS GUARDAMOS EN UN ARREGLO PARA FACILIDAD
+
+            Class<?> clazz = Class.forName(paramsArray[0]); //Obtenemos un objeto de tipo clase que representa la clase a llamar dentro de los parametros generales, EJ: java.lang.Math, java.lang.System
+            String methodName = (paramsArray.length > 1) ? paramsArray[1] : "";//Metodo de la clase dentro de los parametros que queremos ejecutar, EJ: max, sum, abs, getenv
+
+            //Verifico segun el metodo la funcion especifica que debo llamar ya conocienco sus parametros
+            switch (reflectiveMethod) {
+                case "Class":
+                    return obtenerTodo(clazz);
+                case "invoke":
+                    return invocarMetodo(clazz, methodName);
+                case "unaryInvoke":
+                    return invocarMetodoUnario(clazz, methodName, paramsArray[2], paramsArray[3]);
+                case "binaryInvoke":
+                    return invocarMetodoBinario(clazz, methodName, paramsArray[2], paramsArray[3], paramsArray[4], paramsArray[5]);
+                default:
+                    JsonObject errorResponse = new JsonObject();
+                    errorResponse.addProperty("error", "Comando no reconocido");
+                    return errorResponse;
+            }
+        } catch (Exception e) {
+            JsonObject errorResponse = new JsonObject();
+            errorResponse.addProperty("error", "Errorrrrr: " + e.getMessage());
+            return errorResponse;
         }
     }
 
